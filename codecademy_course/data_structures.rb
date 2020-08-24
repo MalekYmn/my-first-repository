9211b3c27fc08287fd833ce22d55a4a7dd2c2c9f
Malek Ymn <malekymn@hotmail.com>
2020-08-24 15:19:17 +0000

    7th lesson of codecademy course -not yet completed

---
+++ codecademy_course/data_structures.rb
@@ -0,0 +1,24 @@
+#part 1
+my_array = [1, 2, 3, 4, 5]
+
+#part2
+demo_array = [100, 200, 300, 400, 500]
+
+print  demo_array[2]
+
+#part 3
+string_array = ["a", "b", "c"]
+
+#part4
+my_2d_array = [["i", "want", "to"], [true,false], [1,2,3,4,5]]
+
+#part5
+pets = Hash.new
+
+#part6
+pets["Malek"] = "rave"
+
+#part7
+puts pets["Malek"]
+
+#part8

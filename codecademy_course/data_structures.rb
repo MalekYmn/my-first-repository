35d58ef4282ae5c1880fc83903d13a32507a3cff
Malek Ymn <malekymn@hotmail.com>
2020-08-25 11:59:04 +0000

    7th codecademy lesson - completed

--- codecademy_course/data_structures.rb
+++ codecademy_course/data_structures.rb
@@ -22,3 +22,36 @@ pets["Malek"] = "rave"
 puts pets["Malek"]
 
 #part8
+languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+languages.each { |element| puts "#{element}" }
+
+#part9
+s = [["ham", "swiss"], ["turkey", "cheddar"], ["roast beef", "gruyere"]]
+
+s.each do |sub_array| 
+  sub_array.each do |element| 
+    puts "#{element}"
+  end
+end
+
+#part 10
+my_array = [[true, false], ["cool", "pool"], [5, 10]]
+
+#part 11
+my_hash = {
+  "greatest" => "malek",
+  "I" => "malek"
+}
+
+#part12
+lunch_order = {
+  "Ryan" => "wonton soup",
+  "Eric" => "hamburger",
+  "Jimmy" => "sandwich",
+  "Sasha" => "salad",
+  "Cole" => "taco"
+}
+
+lunch_order.each do |x, y|
+  puts "#{y}"
+end

ecffd6f260d5cb59bff0ecd44f8af076519b9360
Malek Ymn <malekymn@hotmail.com>
2020-09-10 21:41:53 +0000

    15th ruby lesson

--- codecademy_course/blocks_procs_and_lambdas.rb
+++ codecademy_course/blocks_procs_and_lambdas.rb
@@ -40,3 +40,102 @@ end
 double(5) { |n| puts n * 2 }
 
 #part6
+floats = [1.2, 3.45, 0.91, 7.727, 11.42, 482.911]
+# Write your code below this line!
+round_down = Proc.new { |x| x.floor }
+
+
+# Write your code above this line!
+ints = floats.collect(&round_down)
+print ints
+
+#part7
+
+group_1 = [4.1, 5.5, 3.2, 3.3, 6.1, 3.9, 4.7]
+group_2 = [7.0, 3.8, 6.2, 6.1, 4.4, 4.9, 3.0]
+group_3 = [5.5, 5.1, 3.9, 4.3, 4.9, 3.2, 3.2]
+
+# Complete this as a new Proc
+over_4_feet = Proc.new { |height| height >= 4 }
+
+# Change these three so that they use your new over_4_feet Proc
+can_ride_1 = group_1.select(&over_4_feet)
+can_ride_2 = group_2.select(&over_4_feet)
+can_ride_3 = group_3.select(&over_4_feet)
+
+puts can_ride_1
+puts can_ride_2
+puts can_ride_3
+
+#part8
+def greeter
+  yield
+end
+
+phrase = Proc.new { puts "Hello there!" }
+
+greeter(&phrase)
+
+#part9
+hi = Proc.new { puts "Hello!" }
+hi.call
+
+#part10
+numbers_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+strings_array = numbers_array.map(&:to_s)
+
+puts strings_array
+
+#part11
+strings = ["leonardo", "donatello", "raphael", "michaelangelo"]
+# Write your code below this line!
+
+symbolize = lambda { |x| x.to_sym }
+
+# Write your code above this line!
+symbols = strings.collect(&symbolize)
+print symbols
+
+#part12
+my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages]
+
+# Add your code below!
+symbol_filter = lambda { |s| s.is_a? Symbol }
+symbols = my_array.select(&symbol_filter)
+puts symbols
+
+#part13&14
+odds_n_ends = [:weezard, 42, "Trady Blix", 3, true, 19, 12.345]
+
+# Add your code below!
+ints = odds_n_ends.select{ |x| x.is_a? Integer }
+puts ints
+
+#part14
+ages = [23, 101, 7, 104, 11, 94, 100, 121, 101, 70, 44]
+
+# Add your code below!
+under_100 = Proc.new { |x| x < 100 }
+
+youngsters = ages.select(&under_100)
+puts youngsters
+
+#part15&16
+crew = {
+  captain: "Picard",
+  first_officer: "Riker",
+  lt_cdr: "Data",
+  lt: "Worf",
+  ensign: "Ro",
+  counselor: "Troi",
+  chief_engineer: "LaForge",
+  doctor: "Crusher"
+}
+# Add your code below!
+first_half = lambda do |key, value| 
+               value < "M"
+             end
+             
+a_to_m = crew.select(&first_half)
+puts a_to_m
